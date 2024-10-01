b313ba4680fa85567a8189093046e530b82a7cba
savanp08 <savanpoduturi@gmail.com>
2024-09-30 22:19:28 -0500

    phase 1 network tests

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
     console.log('Received request:', req.method, req.url);
     next();
 });
-app.use(express.static("../"));
+app.use(express.static("./"));
 
 app.get("/test", (req, res) => {
     console.log("Test route accessed");
